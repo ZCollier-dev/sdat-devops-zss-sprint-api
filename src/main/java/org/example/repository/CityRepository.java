95071a383fdb296743a3af53fc4ff04f7344395b
Zack <zacharycollier36@protonmail.com>
2025-06-25 14:43:59 -0230

    Fixed main having old copies of API packages

--- src/main/java/org/example/repository/CityRepository.java
+++
@@ -1,6 +0,0 @@
-package org.example.repository;
-
-import org.example.model.City;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CityRepository extends JpaRepository<City, Long> {}
