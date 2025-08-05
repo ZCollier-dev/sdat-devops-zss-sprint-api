5114d17044aee15e97eafeb570a1e0f4d0df8610
Sarah Murphy <sarahelizabeth.murphy@keyin.com>
2025-08-05 15:09:44 -0230

    Accidentally committed without saving. Adding code for airline classes.

---
+++ src/main/java/org/example/rest/airline/AirlineController.java
@@ -0,0 +1,34 @@
+package org.example.rest.airline;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/airlines")
+public class AirlineController {
+
+    @Autowired
+    private AirlineService airlineService;
+
+    @GetMapping
+    public List<Airline> getAll() {
+        return airlineService.getAllAirlines();
+    }
+
+    @PostMapping
+    public Airline add(@RequestBody Airline airline) {
+        return airlineService.addAirline(airline);
+    }
+
+    @PutMapping("/{id}")
+    public Airline update(@PathVariable Long id, @RequestBody Airline airline) {
+        return airlineService.updateAirline(id, airline);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        airlineService.deleteAirline(id);
+    }
+}
