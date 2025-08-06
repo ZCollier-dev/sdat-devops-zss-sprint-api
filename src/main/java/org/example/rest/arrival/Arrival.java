85945d1a571a866e3bccce6b2836f3c71156427b
Zack <zacharycollier36@protonmail.com>
2025-08-06 15:59:18 -0230

    Added code to all Arrival classes.

--- src/main/java/org/example/rest/arrival/Arrival.java
+++ src/main/java/org/example/rest/arrival/Arrival.java
@@ -1,4 +1,64 @@
 package org.example.rest.arrival;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import org.example.rest.airport.Airport;
+import org.example.rest.flight.Flight;
+
+import java.util.Calendar;
+
+@Entity
 public class Arrival {
+
+    @Id
+    private Long id;
+
+    private Calendar arrivalTime;
+    private String status;
+
+    @OneToOne
+    @JsonIgnore
+    private Flight flight;
+
+    // Origin Airport
+    @OneToMany
+    private Airport airport;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public Calendar getArrivalTime(){
+        return arrivalTime;
+    }
+    public void setArrivalTime(Calendar arrivalTime){
+        this.arrivalTime = arrivalTime;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+    public Flight getFlight(){
+        return flight;
+    }
+    public void setFlight(Flight flight){
+        this.flight = flight;
+    }
+
+    public Airport getAirport(){
+        return airport;
+    }
+    public void setAirport(Airport airport){
+        this.airport = airport;
+    }
 }
--- src/main/java/org/example/rest/arrival/ArrivalService.java
+++ src/main/java/org/example/rest/arrival/ArrivalService.java
@@ -1,4 +1,30 @@
 package org.example.rest.arrival;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ArrivalService {
+
+    @Autowired
+    private ArrivalRepository arrivalRepo;
+
+    public List<Arrival> getAllArrivals(){
+        return arrivalRepo.findAll();
+    }
+
+    public Arrival addArrival(Arrival arrival){
+        return arrivalRepo.save(arrival);
+    }
+
+    public Arrival updateArrival(Long id, Arrival arrival){
+        arrival.setId(id);
+        return arrivalRepo.save(arrival);
+    }
+
+    public void deleteArrival(Long id){
+        arrivalRepo.deleteById(id);
+    }
 }
