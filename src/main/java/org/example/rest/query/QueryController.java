1d6598d75ffd69861fdd8a56ee04a31d2b1ea289
Zack <zacharycollier36@protonmail.com>
2025-08-14 09:57:23 -0230

    Added CrossOrigin to all of the Controller classes.
    I forgot these when making the API.

--- src/main/java/org/example/rest/query/QueryController.java
+++ src/main/java/org/example/rest/query/QueryController.java
@@ -3,11 +3,13 @@ package org.example.rest.query;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class QueryController {
 
