9a8227e96fce389d9925bf58d0a4472166b2ba8c
Zack <zacharycollier36@protonmail.com>
2025-08-14 15:43:41 -0230

    Attempt to fix airline 404 error

--- src/main/java/org/example/rest/airline/AirlineController.java
+++ src/main/java/org/example/rest/airline/AirlineController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/airlines")
+@RequestMapping("/airline")
 public class AirlineController {
 
     @Autowired
