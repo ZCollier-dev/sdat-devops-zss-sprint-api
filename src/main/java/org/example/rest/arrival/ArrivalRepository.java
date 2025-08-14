1c13945dc73fe973aedf36a321f7533e2956d16c
Zack <zacharycollier36@protonmail.com>
2025-08-14 16:03:26 -0230

    added repository annotations to all repositories, i don't know how any of this worked without

--- src/main/java/org/example/rest/arrival/ArrivalRepository.java
+++ src/main/java/org/example/rest/arrival/ArrivalRepository.java
@@ -1,5 +1,7 @@
 package org.example.rest.arrival;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArrivalRepository extends JpaRepository<Arrival, Long> {}
--- src/main/java/org/example/rest/gate/GateRepository.java
+++ src/main/java/org/example/rest/gate/GateRepository.java
@@ -1,9 +1,11 @@
 package org.example.rest.gate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface GateRepository extends JpaRepository<Gate, Long> {
     List<Gate> findByAirportId(Long airportId);
 }
